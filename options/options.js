2aa878c7a4ad40ff9cde075e76a37171406dbe13
Crowdbotics <team@crowdbotics.com>
2022-04-19 16:07:11 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "lsi_customer_portal_34833",
+  url: "https://lsi_customer_portal_34833.botics.co",
+  api: "https://lsi_customer_portal_34833.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}

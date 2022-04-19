f76f63308d2ab9258e0f83d558b3c69b42a95e1b
Crowdbotics <team@crowdbotics.com>
2022-04-19 16:11:36 +0000

    Update options

--- options/options.js
+++ options/options.js
@@ -1,12 +1,25 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet } from "react-native"
 
 export const globalOptions = {
   name: "lsi_customer_portal_34833",
   url: "https://lsi_customer_portal_34833.botics.co",
   api: "https://lsi_customer_portal_34833.botics.co/api/v1"
 }
+
 export const modulesOptions = {
   "@modules/app-menu": {
-    "copy": "Routes available!"
+    copy: "Routes available!"
+  },
+  "@modules/login": {
+    LOGO_URL:
+      "https://crowdbotics-slack-dev.s3.amazonaws.com/media/project_component_resources/cb-icon.png",
+    BACKGROUND_URL:
+      "https://crowdbotics-slack-dev.s3.amazonaws.com/media/project_component_resources/halfbg.png",
+    HOME_SCREEN_NAME: "Login",
+    validateEmail: "^[^\\s]+([.-]?\\w+)*@\\w+([.-]?\\w+)*(.\\w{2,3})+$",
+    SignInNavText: "Sign In",
+    SignUpNavText: "Sign Up",
+    SignInButtonText: "Login",
+    SignUpButtonText: "Sign Up"
   }
 }
